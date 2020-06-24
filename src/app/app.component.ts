c94efabdeaea8ca53cd64d9aee9e61bb50e9cb90
luk <lalalalaluk@gmail.com>
2020-06-24 15:43:56 +0800

    lesson1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'angular-tutorial';
+  name = 'Hello Angular';
+  trigger = false;
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  buttonClick() {
+    if (this.trigger === true) {
+      this.trigger = false;
+    } else {
+      this.trigger = true;
+    }
+  }
+
+  test1Click() {
+    this.router.navigate(['test1']);
+  }
+
+  test2Click() {
+    this.router.navigate(['test2']);
+  }
 }
